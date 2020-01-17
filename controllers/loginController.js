aff7309987ee204c9e98c895e6e4c6f62499fdee
Ashu Kumar Sharma <ashu.sharma@tothenew.com>
2020-01-17 12:21:31 +0530

    updated

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,7 +1,7 @@
 const Post = require('../models/post');
 const jwt = require('jsonwebtoken');
-// const JWT_SECRET = require('../constants/constant.js');
-// const frontendUrl = require('../constants/constant.js');
+const JWT_SECRET = require('../constants/constant.js');
+const frontendUrl = require('../constants/constant.js');
 
 module.exports = {
         googleRedirect: (req,res)=>{
@@ -9,10 +9,11 @@ module.exports = {
             console.log('heerererer');
             if(req && req.user && req.user.success){
                 let userID = req.user._doc._id.toJSON();
-                token = jwt.sign(userID, process.env.JWTSECRET);
+                token = jwt.sign(userID, JWT_SECRET.jwtSecret.secret);
                 res.cookie("token",token);
                 console.log('heerererer');
-                res.redirect(process.env.FRONTENDURL);
+                res.redirect(frontendUrl.frontendUrl.url);
+                // res.redirect(process.env.FRONTENDURL);
             }
             else{
                 res.redirect(process.env.FRONTENDURL);
