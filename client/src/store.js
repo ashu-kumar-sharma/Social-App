e5715c3747b1061551d6dc65e5dcef49a25c22b9
Ashu Kumar Sharma <ashu.sharma@tothenew.com>
2020-01-27 14:51:21 +0530

    something

--- client/src/store.js
+++ client/src/store.js
@@ -2,9 +2,7 @@ import {applyMiddleware, createStore, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './Reducer/rootReducer';
 
-const store = createStore(rootReducer,  compose(applyMiddleware(thunk),
-                    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                )
+const store = createStore(rootReducer,  compose(applyMiddleware(thunk))
     );
 // store.subscribe(() => {
 // console.log("Store has changed", store.getState());
